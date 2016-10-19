6b2433a6a502ff21ffa82a03e143c51936151fa2
z2oh <jadaytime@gmail.com>
2016-10-19 18:13:52 -0400

    spaces -> tabs

--- src/app.ts
+++ src/app.ts
@@ -8,30 +8,30 @@ var BrowserWindow = electron.BrowserWindow;
 var mainWindow;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        width: 600,
-        height: 300,
-        'min-width': 500,
-        'min-height': 200,
-        'accept-first-mouse': true,
-        'title-bar-style': 'hidden'
-    });
-    mainWindow.loadURL("file://" + __dirname + "/index.html");
-    mainWindow.on('closed', function () {
-        mainWindow = null;
-    });
+	mainWindow = new BrowserWindow({
+		width: 600,
+		height: 300,
+		'min-width': 500,
+		'min-height': 200,
+		'accept-first-mouse': true,
+		'title-bar-style': 'hidden'
+	});
+	mainWindow.loadURL("file://" + __dirname + "/index.html");
+	mainWindow.on('closed', function () {
+		mainWindow = null;
+	});
 }
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', function () {
-    if (process.platform != 'darwin') {
-        app.quit();
-    }
+	if (process.platform != 'darwin') {
+		app.quit();
+	}
 });
 
 app.on('activate', function () {
-    if (mainWindow === null) {
-        createWindow();
-    }
+	if (mainWindow === null) {
+		createWindow();
+	}
 });
