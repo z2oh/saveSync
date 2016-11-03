91f8e0b085d6cfad53984d462f101fd7f001d355
z2oh <jadaytime@gmail.com>
2016-11-02 23:06:16 -0400

    added basic unit tests and travis ci

---
+++ test/main.spec.js
@@ -0,0 +1,28 @@
+// A simple test to verify a window is opened with the title 'saveSync'
+var Application = require('spectron').Application;
+var assert = require('assert');
+var electron = require('electron');
+
+describe('application lanching', function () {
+	this.timeout(10000);
+
+	beforeEach(function () {
+		this.app = new Application({
+			path: electron,
+			args: ['build/app.js']
+		});
+		return this.app.start();
+	});
+
+	afterEach(function () {
+		if (this.app && this.app.isRunning()) {
+			return this.app.stop();
+		}
+	});
+
+	it('should launch with the title', function () {
+		return this.app.client.getTitle().then(function (title) {
+			assert.equal(title, 'saveSync');
+		});
+	});
+});
